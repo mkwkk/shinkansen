d88d55035116e2b7391424d4c46d4d5e173fcf12
thuptm <thuptm@cystack.net>
2022-09-01 13:42:09 +0700

    update heroku

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from website import create_app
+import os
+
+port = int(os.environ.get("PORT", 5000))
 app = create_app()
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host = '0.0.0.0', port = port, debug = True)
